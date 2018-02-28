60fdbf5aee485a63822bfb9224f2b2ea63cf56fb
Joesdev <joesilvadev@gmail.com>
2018-02-28 05:57:56 +0000

    Created function 'notifyUsersEmailOfPetArrival' added partial funcitonality

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Notifications\Notifiable;
+
 
 class NotificationController extends Controller
 {
-    //
+    use Notifiable;
+
+    public function notifyUsersEmailOfPetArrival($email, $pet){
+        // This functions follows a function which determines a pet has arrived
+        $invoice = 'am i a message of some kind?';
+        Notification::route('mail', 'joesilvpb4@gmail.com')->notify(new InvoicePaid($invoice));
+    }
 }
