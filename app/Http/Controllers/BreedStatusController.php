0c2291d1d3fc8880a965bee785bb6b213f45ff4a
Joesdev <joesilvadev@gmail.com>
2018-03-10 05:50:09 +0000

    Modified breeds.js

--- app/Http/Controllers/BreedStatusController.php
+++ app/Http/Controllers/BreedStatusController.php
@@ -10,12 +10,6 @@ class BreedStatusController extends Controller
     public function getAllBreeds(){
         $breedText = Storage::disk('local')->get('/data/breeds.json');
         $breedArray = json_decode($breedText, true);
-        $index = 0;
-        // Turns BreedArray from nested to single level array
-        foreach($breedArray as $item){
-            $breedArray[$index] = $item['$t'];
-            $index++;
-        };
         return $breedArray;
     }
 
