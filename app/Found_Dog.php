4d3f01965b5203b778014171c1da22a9f0f20d06
Joesdev <joesilvadev@gmail.com>
2018-03-24 21:32:21 +0000

    Added model for found_dogs table

---
+++ app/Found_Dog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Found_Dog extends Model
+{
+    public $timestamps = true;
+    public $primaryKey = 'id';
+
+    protected $fillable = ['email', 'new_breed_id','miles'];
+
+}
