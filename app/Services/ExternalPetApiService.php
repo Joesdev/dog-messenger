6a3621004a68fc3634835b0293a751917221a5b0
Joesdev <joesilvadev@gmail.com>
2018-04-18 00:44:13 +0000

    Finished validation functions for getSlimDogData function

--- app/Services/ExternalPetApiService.php
+++ app/Services/ExternalPetApiService.php
@@ -32,36 +32,29 @@ class ExternalPetApiService
         );
         $data = json_decode($response->getBody()->getContents(), true);
         $data = $data['petfinder']['pet'];
-        $this->getSlimData($data);
+        $this->getSlimDogData($data);
         return $data;
     }
 
-    public function getSlimData($dogData)
+    public function getSlimDogData($dogData)
     {
-
-            $dogData['mix'] = [];
-            //dd($dogData);
             $masterArrayOfDogs = [
+                'name'        => $this->validateKey('name', $dogData),
+                'age'         => $this->validateKey('age', $dogData),
+                'size'        => $this->validateKey('size', $dogData),
+                'sex'         => $this->validateKey('sex', $dogData),
+                'mix'         => $this->validateKey('mix', $dogData),
                 'description' => $this->validateKey('description', $dogData),
-                'name' => $this->validateKey('name', $dogData),
-                'age'  => $this->validateKey('age', $dogData),
-                'size'  => $this->validateKey('size', $dogData),
-                'sex'  => $this->validateKey('sex', $dogData),
-                'mix' => $this->validateKey('mix', $dogData)
-                /*'phone' => $dogData['contact']['phone']['$t'],
-                'email' => $dogData['contact']['email']['$t'],
-                'address' => $dogData['contact']['address1']['$t'],
-                'city' => $dogData['contact']['city']['$t'],*/
-                //'distance' => $found_dogs[$index]['miles'] . ' miles',
+                'phone'       => $this->validateContactKey('phone', $dogData),
+                'email'       => $this->validateContactKey('email', $dogData),
+                'address'     => $this->validateContactKey('address1', $dogData),
+                'city'        => $this->validateContactKey('city', $dogData),
+                'state'       => $this->validateContactKey('state', $dogData),
+                'media'       => $this->validateMediaKey(500, $dogData),
+                //'distance'   => $found_dogs[$index]['miles'] . ' miles',
             ];
 
-            /*foreach($dogData['media']['photos']['photo'] as $photo){
-                if(strpos($photo['$t'], 'width=500')){
-                    $masterArrayOfDogs['media'] = $photo['$t'];
-                    break;
-                }
-            };*/
-            dd($masterArrayOfDogs);
+            return $masterArrayOfDogs;
 
     }
 
@@ -79,7 +72,26 @@ class ExternalPetApiService
 
     public function validateContactKey($key, $data)
     {
+        $accepted_keys = ['address1','city','email','phone', 'state'];
+        if(in_array($key, $accepted_keys, true)) {
+            if(!empty($data['contact'][$key])){
+                return $data['contact'][$key]['$t'];
+            } else {
+                return 'Not Available';
+            }
+        }
+    }
 
+    public function validateMediaKey($width, $data)
+    {
+        if(!empty($data['media']['photos']['photo'])) {
+            foreach ($data['media']['photos']['photo'] as $photo) {
+                if (strpos($photo['$t'], 'width=' . $width)) {
+                    return $photo['$t'];
+                }
+            };
+        }
+        return '#';
     }
 
 }
