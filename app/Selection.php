d2b7f5a3273da40a4f657c22a2250a7b3a1b7ebd
Joesdev <joesilvadev@gmail.com>
2018-05-28 02:12:09 +0000

    made id of selections fillable

--- app/Selection.php
+++ app/Selection.php
@@ -9,7 +9,7 @@ class Selection extends Model
     public $timestamps = true;
     public $primaryKey = 'id';
 
-    protected $fillable = ['breed_id', 'zip','highest_breed_id', 'max_miles', 'match'];
+    protected $fillable = ['id','breed_id', 'zip','highest_breed_id', 'max_miles', 'match'];
 
     public function users(){
         return $this->hasOne('App\User', 'selection_id','id');
