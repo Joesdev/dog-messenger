a07fea6d608878dab20bb57396fed9fdfa2a74dd
Joesdev <joesilvadev@gmail.com>
2018-08-06 01:05:53 +0000

    added assertions to showCollectedArrayOfDogsView

--- tests/Unit/BreedControllerTest.php
+++ tests/Unit/BreedControllerTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature;
 
 use App\Found_Dog;
+use App\User as UserModel;
+use App\Selection as SelectionModel;
+
 use App\Http\Controllers\BreedController;
-use App\Services\UserService as User;
 use Tests\TestCase;
+use App\Services\UserService as UserService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class BreedControllerTest extends TestCase
@@ -23,15 +26,17 @@ class BreedControllerTest extends TestCase
     public function setUp(){
         parent::setUp();
         $this->seed('found_dogsTableSeeder');
-        factory(User::class,1)->create(['email' => $this->email]);
         $this->breedController = new BreedController(new UserService());
     }
 
     public function test_showCollectedArrayOfDogsView_returns_view_with_data_from_found_dogs_table()
-    {   $countOfRows = Found_Dog::whereEmail($this->email)->count();
+    {
+        factory(UserModel::class,1)->create(['email' => $this->email]);
+        $countOfRows = Found_Dog::whereEmail($this->email)->count();
         $response = $this->get('/results/'.$this->email);
         $data = $response->getOriginalContent()->getData();
         $this->assertCount($countOfRows,$data['dogData']);
+        $this->assertArrayHasKey('userSelection', $data);
     }
 
     public function test_redirectInvalidEmail_redirects_to_home_page_when_email_is_not_in_found_dogs_table()
