431bc8b04f49be324af8cf93ea99c91315d9fd16
Joesdev <joesilvadev@gmail.com>
2018-08-06 20:22:20 +0000

    Added truncate:dogs as an artisan command

---
+++ app/Console/Commands/deleteExpiredFoundDogs.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Found_Dog;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class deleteExpiredFoundDogs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'truncate:dogs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Removes rows in found_dogs table which have exceeded the time limit';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Found_Dog::where('created_at', '<',Carbon::now())->each(function($item){
+           $item->delete();
+        });
+    }
+}
