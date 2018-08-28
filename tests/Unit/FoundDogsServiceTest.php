b917001438d82978365d462b046a8da36eb0f6c3
Joesdev <joesilvadev@gmail.com>
2018-08-28 09:32:33 +0000

    finished test for FoundDogsService@deleteRecord

--- tests/Unit/FoundDogsServiceTest.php
+++ tests/Unit/FoundDogsServiceTest.php
@@ -4,16 +4,19 @@
 namespace Tests\Unit;
 
 use App\Found_Dog;
+use App\Services\FoundDogsService;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class FoundDogsServiceTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_deleteRecord_removes_correct_amount_of_records()
     {
-        //Given I have records in the found dogs table
-        $found_dogs = factory(Found_Dog::class,3)->create();
-        //When I call the function
-        //Then I should have the initial count less the amount of deleted records
+        $service = new FoundDogsService();
+        $found_dog = factory(Found_Dog::class,3)->create()->first();
+        $service->deleteRecord($found_dog->new_breed_id);
+        $this->assertCount(2, Found_Dog::all());
     }
 }
