d474088dcd8f9f8cbc23a09a6922125a6653f830
Joesdev <joesilvadev@gmail.com>
2018-08-28 09:37:10 +0000

    Refactored deleteRecord test

--- tests/Unit/FoundDogsServiceTest.php
+++ tests/Unit/FoundDogsServiceTest.php
@@ -12,11 +12,19 @@ class FoundDogsServiceTest extends TestCase
 {
     use RefreshDatabase;
 
+    protected $service;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->service = new FoundDogsService();
+    }
+
     public function test_deleteRecord_removes_correct_amount_of_records()
     {
-        $service = new FoundDogsService();
-        $found_dog = factory(Found_Dog::class,3)->create()->first();
-        $service->deleteRecord($found_dog->new_breed_id);
+        $found_dogs = factory(Found_Dog::class,3)->create();
+        $found_dog = $found_dogs->first();
+        $this->service->deleteRecord($found_dog->new_breed_id);
         $this->assertCount(2, Found_Dog::all());
     }
 }
