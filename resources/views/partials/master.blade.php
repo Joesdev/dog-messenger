26d10511101aeb636a1154ae4135fdf5f373e289
cassiea12 <c.amantite@gmail.com>
2018-08-29 16:35:16 +0800

    edited google search results preview

--- resources/views/partials/master.blade.php
+++ resources/views/partials/master.blade.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content='{{ csrf_field() }}'>
-    <title>Dog Finder</title>
+    <title>Find A Shelter Puppy Near You</title>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-     <title>Find a dog to adopt near you</title>
-    <meta name="description" content="In 3 easy steps, find a dog to adopt from an animal shelter near you based on the breed you are looking for and your location">
+     <title>Find a shelter puppy to adopt near you</title>
+    <meta name="description" content="Be the first to find out when a new puppy is brought into an animal shelter near you!">
 </head>
 <body>
 @yield('content')
